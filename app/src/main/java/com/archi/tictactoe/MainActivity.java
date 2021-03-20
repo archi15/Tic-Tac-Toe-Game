2d31a24ae1317e3b34ae81970771cbdf855f532f
Archi Aggarwal <archiaggarwal2002@gmail.com>
2021-03-20 15:58:24 +0530

    Update Repo

---
+++ app/src/main/java/com/archi/tictactoe/MainActivity.java
@@ -0,0 +1,105 @@
+package com.archi.tictactoe;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    boolean gameActive = true;
+    //0 - X
+    //1 - O
+    //2 - NULL
+    int activePlayer = 0;
+    int[] gameState = {2, 2, 2, 2, 2, 2, 2, 2, 2};
+
+    int[][] winningPositions = {{0,1,2}, {3,4,5}, {6,7,8},
+                            {0,3,6}, {1,4,7}, {2,5,8},
+                            {0,4,8}, {2,4,6}};
+    public void playerTap(View view){
+        ImageView img = (ImageView) view;
+        int tappedImage = Integer.parseInt(img.getTag().toString());
+//        if(!gameActive){
+//            gameReset(view);
+//        }
+        if(gameState[tappedImage] == 2 && gameActive) {
+            gameState[tappedImage] = activePlayer;
+            img.setTranslationY(-1000f);
+            if (activePlayer == 0) {
+                img.setImageResource(R.drawable.x);
+                activePlayer = 1;
+                TextView status = findViewById(R.id.status);
+                status.setText("O's turn - Tap to play");
+            }
+            else {
+                img.setImageResource(R.drawable.o);
+                activePlayer = 0;
+                TextView status = findViewById(R.id.status);
+                status.setText("X's turn - Tap to play");
+            }
+            img.animate().translationYBy(1000f).setDuration(300);
+        }
+        //check if any player has won!
+        for(int[] winPosition: winningPositions){
+            if(gameState[winPosition[0]] == gameState[winPosition[1]] &&
+                gameState[winPosition[1]] == gameState[winPosition[2]] &&
+                gameState[winPosition[0]] != 2){
+                String winner;
+                gameActive = false;
+                if(gameState[winPosition[0]] == 0){
+                    winner = "X has won!";
+                }
+                else {
+                    winner = "O has won!";
+                }
+                TextView status = findViewById(R.id.status);
+                status.setText(winner);
+            }
+        }
+        boolean emptySquare = false;
+        for (int squareState : gameState) {
+            if (squareState == 2) {
+                emptySquare = true;
+                break;
+            }
+        }
+        if (!emptySquare && gameActive) {
+            // Game is a draw
+            gameActive = false;
+            String winnerStr;
+            winnerStr = "It's draw!";
+            TextView status = findViewById(R.id.status);
+            status.setText(winnerStr);
+        }
+    }
+    public void tapReplay(View view){
+//        if(!gameActive){
+            gameReset(view);
+//        }
+    }
+    public void gameReset(View view){
+        gameActive = true;
+        activePlayer = 0;
+        for(int i=0; i<gameState.length; i++){
+            gameState[i] = 2;
+        }
+        ((ImageView)findViewById(R.id.imageView0)).setImageResource(0);
+        ((ImageView)findViewById(R.id.imageView1)).setImageResource(0);
+        ((ImageView)findViewById(R.id.imageView2)).setImageResource(0);
+        ((ImageView)findViewById(R.id.imageView3)).setImageResource(0);
+        ((ImageView)findViewById(R.id.imageView4)).setImageResource(0);
+        ((ImageView)findViewById(R.id.imageView5)).setImageResource(0);
+        ((ImageView)findViewById(R.id.imageView6)).setImageResource(0);
+        ((ImageView)findViewById(R.id.imageView7)).setImageResource(0);
+        ((ImageView)findViewById(R.id.imageView8)).setImageResource(0);
+        TextView status = findViewById(R.id.status);
+        status.setText("X's turn - Tap to play");
+    }
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
